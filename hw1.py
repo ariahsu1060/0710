6c408d981239df2d380d9fe1a5820e30957cf884
123 <123@example.com>
2025-07-10 16:19:19 +0800

    receiver

--- hw1.py
+++ hw1.py
@@ -5,23 +5,17 @@ from email.header import Header
 
 msg = MIMEMultipart()
 sender ="aria.hsu1060@gmail.com"
-receiver="juanx20020110@gmail.com"
+receiver=["juanx20020110@gmail.com"]
 msg["fROM"] = sender
-msg["TO"]= receiver
+msg["TO"]= receiver[0]
 msg["Subject"] = Header("test mail","utf-8").encode()
 body = "this is mail from python"
 msg_content = MIMEText(body,_charset="utf-8")
 msg.attach(msg_content)
 ssl_context = ssl.create_default_context()
-"""
-server = smtplib.SMTP_SSL("smtp.gmail.com",465,context=ssl_context)
-server.login(sender,"yiyw vflg yydx zvjk")
-server.sendmail(sender,receiver,msg.as_string())
-server.close()
-print("success send mail")
-"""
+
 with smtplib.SMTP_SSL("smtp.gmail.com",465,context=ssl_context) as server:
     server.login(sender,"yiyw vflg yydx zvjk")
-    server.sendmail(sender,receiver,msg.as_string())
+    server.sendmail(sender,receiver[0],msg.as_string())
 
 print("success send mail")
